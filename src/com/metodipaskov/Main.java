3ec4b8e5cd2f3a92c2593a6427d724857738c0fd
paskovm <metodi_paskov@yahoo.com>
2023-01-22 11:11:58 +0200

    Initial commit

---
+++ src/com/metodipaskov/Main.java
@@ -0,0 +1,23 @@
+package com.metodipaskov;
+
+public class Main {
+
+    /*
+    * Admin:
+    * admin, admin
+    *
+    * Librarian:
+    * "emcneilly0@wordpress.com", "UOIBTReU1L"
+    *
+    * Clerk:
+    * "lsoloway1@cam.ac.uk", "e9gIIOTBFcK"
+    *
+    * Borrower:
+    * "glicciardi9@a8.net", "aPxZI4vPn"
+    * */
+
+    public static void main(String[] args) {
+
+//        new MainMenu().start();
+    }
+}
